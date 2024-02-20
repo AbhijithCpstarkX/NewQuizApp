31070ec4aa2af31800a62fa97e2b09294fa0e507
Asheeqa-RD <160301332+AbhijithCpstarkX@users.noreply.github.com>
2024-02-20 17:06:38 +0530

    gsdg
    hhhh

--- quizapp/lib/home.dart
+++ quizapp/lib/home.dart
@@ -12,6 +12,40 @@ class _HomeAppState extends State<HomeApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
+      body: Center(
+        child:
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [Text('All dogs have tails. ',style: TextStyle(color: Colors.white,fontSize: 50,),),
+          SizedBox(
+            height: 200,
+          ),
+          TextButton(
+            style: TextButton.styleFrom(
+              fixedSize: Size(200, 50),
+                backgroundColor: Colors.green,
+                textStyle: const TextStyle(fontSize: 20)),
+            onPressed: () {},
+            child: const Text('TRUE',style: TextStyle(color: Colors.white)),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          TextButton(
+            style: TextButton.styleFrom(
+                fixedSize: Size(200, 50),
+                backgroundColor: Colors.red,
+                textStyle: const TextStyle(fontSize: 20)),
+            onPressed: () {},
+            child: const Text('FALSE',style: TextStyle(color: Colors.white)),
+          ),
+
+
+
+        ],
+      ),
+      ),
     );
   }
 }
